ffe9dcab8e15997b1ccd7448b59b7e63faca4809
Alin Stelian <stelian.alin@yahoo.com>
2024-03-07 21:20:39 +0200

    update tests

--- demoTests/src/test/java/com/ddf/test/FastPassFailZephyrAnnotationTest.java
+++ demoTests/src/test/java/com/ddf/test/FastPassFailZephyrAnnotationTest.java
@@ -1,6 +1,7 @@
 package com.ddf.test;
 
 import com.dff.listeners.TestListener;
+import com.ind.dw.zephyr.annotation.ZephyrTestCase;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -8,11 +9,13 @@ import static org.testng.Assert.assertTrue;
 
 @Listeners({TestListener.class})
 public class FastPassFailZephyrAnnotationTest {
+    @ZephyrTestCase("TES-T400")
     @Test(testName = "Fast Passing Test", description = "Fast pass with known testcaseid as annotation")
     public void fastPassKnownTestCaseIdTest() {
         assertTrue(true);
     }
 
+    @ZephyrTestCase("TES-T401")
     @Test(testName = "Fast Passing Test", description = "Fast pass with not known testcaseid as annotation")
     public void fastPassNotKnownTestCaseIdTest() {
         assertTrue(true);
